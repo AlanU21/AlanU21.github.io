a008031b4dd85c309f28bc507340909ed5abdaf4
Alan <alan.uthuppan@temple.edu>
2024-09-01 16:25:25 -0400

    next.config assetPrefix fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    output: 'export',
+    assetPrefix: isProd ? '/AlanU21.github.io/' : './',
+    trailingSlash: true,
+};
 
 export default nextConfig;
