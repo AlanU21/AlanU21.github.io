f1015334e521ef1f842a5c0ec8ea4da8c6635a2e
Alan <alan.uthuppan@temple.edu>
2024-09-01 18:06:26 -0400

    Build version assetPrefix change

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
     output: 'export',
-    assetPrefix: isProd ? '/AlanU21.github.io/' : '/',
+    assetPrefix: '/',
     trailingSlash: true,
 };
 
