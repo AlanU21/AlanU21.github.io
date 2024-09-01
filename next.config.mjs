3c52a667989096edf96513789fc3bbc228e9cbd1
Alan <alan.uthuppan@temple.edu>
2024-09-01 18:19:36 -0400

    Change back

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: 'export',
-    assetPrefix: '/',
+    assetPrefix: isProd ? '/AlanU21.github.io/' : '/',
     trailingSlash: true,
 };
 
